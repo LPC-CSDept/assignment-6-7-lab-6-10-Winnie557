362bab5e687e8699363c483eac64cec81c007de2
Winnie557 <110578029+Winnie557@users.noreply.github.com>
2022-10-06 06:43:56 +0000

    commit

--- A67.hpp
+++ A67.hpp
@@ -1,5 +1,4 @@
 //
-
 #include <iomanip>
 #include <iostream>
 using namespace std;
@@ -7,3 +6,36 @@ using namespace std;
 // complete the Overloading function swap( )
 // swap ( ) function has the difference number of parameters from 2 to 4
 // For example, swap(10, 20); swap(10, 20, 30); swap(10, 20, 30, 40)
+void swap(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp; // temp is temporary variable. 
+}
+void swap(int &a, int &b, int &c){ 
+    int min, medium, max;
+    if(a > b && a > c){    
+        max = a;
+        medium = (b > c ? b : c);  // If b > c is true, b is the medium. If not, c is the medium.
+        min = (b > c ? c : b); // conditonal operator. 
+    }
+    else if (b > a && b > c){ 
+        max = b;
+        medium = (a>c ? a : c);
+        min = (a>c? c : a);
+    }
+    else{
+        max = c;
+        medium = (a>b ? a : c);
+        min = (a>b ? b : a);
+    }
+    // a = min;
+    // b = medium;
+    // c = max; 
+}
+void swap(int &a, int &b, int &c, int &d){
+    int temp = a;
+    a = b;
+    b = c;
+    c = d;
+    d = temp;
+}
